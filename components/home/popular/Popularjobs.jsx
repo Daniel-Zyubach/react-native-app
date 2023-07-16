3ce70bb5ff18089cbc10e39a85d5849b1a35fd82
Daniel <daniel.e.zyubach@gmail.com>
2023-07-16 15:14:11 +0300

    layout

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
+import { useRouter } from 'expo-router'
 
 import styles from './popularjobs.style'
+import { SIZES, COLORS } from '../../../constants'
+
+import PopularJobCard from '../../common/cards/popular/PopularJobCard'
 
 const Popularjobs = () => {
+  const router = useRouter()
+  //*CHECK* важная часть
+  const isLoading = false
+  const err = false
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={ styles.container } >
+
+      <View style={ styles.header } >
+        <Text style={ styles.headerTitle } >Популярные вакансии</Text>
+        <TouchableOpacity><Text style={ styles.headerBtn } >Показать всё</Text></TouchableOpacity>
+      </View>
+
     </View>
   )
 }
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,65 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
 
 import styles from './welcome.style'
+import { SIZES, icons } from '../../../constants'
+
+const jobTypes = ['Full-time', 'Part-time', 'Contractor']
 
 const Welcome = () => {
+  const router = useRouter()
+  const [activeJobType, setActiveJobType] = useState('Full-time')
+
   return (
     <View>
-      <Text>Welcome</Text>
+      
+      <View style={ styles.container } /* приветсвие */ >
+
+        <Text style={ styles.userName } >Привет!</Text>
+        <Text style={ styles.welcomeMessage } >Найди свою идеальную работу</Text>
+
+      </View>
+
+      <View style={ styles.searchContainer } /* форма поиска */ >
+
+        <View style={ styles.searchWrapper } /* ввод */ >
+          <TextInput style={ styles.searchInput }
+            placeholder='Какую работу ты ещешь?'
+            value=''
+          />
+        </View>
+
+        <TouchableOpacity style={ styles.searchBtn } /* кнопка поиска */
+          onPress={() => {}}
+        >
+          <Image source={ icons.search } resizeMode='contain' style={ styles.searchBtnImage } />
+        </TouchableOpacity>
+
+      </View>
+
+      <View style={ styles.tabsContainer /* вкладки-фильтры */ }>
+
+        <FlatList
+          data={ jobTypes }
+          renderItem={({ item }) => (
+            <TouchableOpacity style={ styles.tab(activeJobType, item) }
+              onPress={() => {
+                setActiveJobType(item)
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text style={ styles.tabText(activeJobType, item) } >{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+
+      </View>
+
     </View>
   )
 }
