f6a8c380f9cf6fced1232f93dedc132441775267
Daniel <daniel.e.zyubach@gmail.com>
2023-07-16 12:04:24 +0300

    CUSTOM-FONTS

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,28 @@
 import { Stack } from 'expo-router'
 
+//подключение шрифтов
+import { useCallback } from 'react'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+SplashScreen.preventAutoHideAsync()
+
 const Layout = () => {
-  return <Stack />
+  //загрузка шрифтов
+  const [fontsLoaded] = useFonts({
+    //импорт шрифтов
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf')
+  })
+  //вывод шрифтов на экран
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+  if (!fontsLoaded) return null
+
+  return <Stack onLayout={ onLayoutRootView } />
 }
 
 export default Layout
