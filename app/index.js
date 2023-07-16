cd697053ea09eb2d42f13d3f737f3f8e9a32ded8
Daniel <daniel.e.zyubach@gmail.com>
2023-07-16 10:23:53 +0300

    preparation

--- app/index.js
+++ app/index.js
@@ -1,10 +1,24 @@
-import { View, Text } from "react-native"
+//импорт состояния
+import { useState } from "react"
+//импорт реакт нэтив компонентов
+import { View, Text, ScrollView, SafeAreaView } from "react-native"
+//импорт навигации
+import { Stack, useRouter } from "expo-router"
+
+//импорт оформления
+import { SIZES, COLORS, icons, images } from '../constants'
+//импорт функциональных компонентов приложения
+import { ScreenHeaderBtn, Welcome, Popularjobs, Nearbyjobs } from '../components'
 
 const Home = () => {
+  //определяем маршрутизатор
+  const router = useRouter()
+
   return (
-    <View>
-      <Text>Hello, world!</Text>
-    </View>
+    //оборачивание в безопасную зону видимости
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+
+    </SafeAreaView>
   )
 }
 
